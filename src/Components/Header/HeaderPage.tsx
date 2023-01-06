b17dc1415210f39ed13d21f8e658a9852d3a7f1f
Wilker Rwaik <wilkermid@gmail.com>
2023-01-06 15:53:13 -0300

    feat: Organizando pastas e criando Header

---
+++ src/Components/Header/HeaderPage.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container } from './styles';
+import Grid from '@mui/material/Grid';
+
+const HeaderPage = () => {
+  return (
+    <Container>
+        <Grid container spacing={2}>
+            <Grid item xs={2}>
+                
+            </Grid>
+            <Grid item xs={8}>
+
+            </Grid>
+            <Grid item xs={2}>
+
+            </Grid>
+        </Grid>
+    </Container>
+  )
+}
+
+export default HeaderPage
--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import HeaderPage from '../../Components/Header/HeaderPage'
 import {Container} from "./styles"
 
 const HomePage = () => {
   return (
     <Container>
-        
+        <HeaderPage/>
     </Container>
   )
 }
