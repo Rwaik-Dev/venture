3e2d6087999bfa6ec33b1c9751c30094e905ae74
Wilker Rwaik <wilkermid@gmail.com>
2023-01-06 15:11:09 -0300

    fix: Instalando bibliotecas e começando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import HomePage from './Pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      
+      <HomePage/>
     </div>
   );
 }
