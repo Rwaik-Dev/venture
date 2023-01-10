707780c660190a535a908de58bc3ba5d58a81c16
Wilker Rwaik <wilkermid@gmail.com>
2023-01-10 08:55:09 -0300

    fix: Removendo Material UI do projeto

--- src/Components/Header/HeaderPage.tsx
+++ src/Components/Header/HeaderPage.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import { Container } from './styles';
-import Grid from '@mui/material/Grid';
 
 const HeaderPage = () => {
   return (
     <Container>
-        <Grid container spacing={2}>
-            <Grid item xs={2}>
-                
-            </Grid>
-            <Grid item xs={8}>
-
-            </Grid>
-            <Grid item xs={2}>
-
-            </Grid>
-        </Grid>
     </Container>
   )
 }
