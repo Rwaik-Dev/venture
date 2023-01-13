d0ff99c637ead8422060b8c24edae836d608308b
Wilker Rwaik <wilkermid@gmail.com>
2023-01-13 11:00:15 -0300

    feat: Criação CardValores finalizado

--- src/Components/CardValores/CardValores.tsx
+++ src/Components/CardValores/CardValores.tsx
@@ -1,18 +1,95 @@
 import React from "react";
-import { Container, Content, Image, TitleContent, TextContent } from "./styles";
+import {
+  Container,
+  Content,
+  Image,
+  TitleContent,
+  TextContent,
+  Separator,
+} from "./styles";
 
 const CardValores = () => {
   return (
     <Container>
+      <TitleContent>Missão, visão e valores</TitleContent>
       <Content>
-        <Image />
-        <TitleContent></TitleContent>
-        <TextContent></TextContent>
+        <Image src="https://www.pontotel.com.br/wp-content/uploads/2020/07/missao-visao-valores-introducao.jpg" />
+        <Separator>
+          <TitleContent>Missão</TitleContent>
+          <TextContent>
+            A Venture Transporte tem como missão oferecer soluções de transporte
+            com segurança, agilidade e eficiência, visando o bem-estar de seus
+            clientes e colaboradores. Queremos nos tornar a primeira opção de
+            transporte para nossos clientes, oferecendo serviços de qualidade,
+            segurança e confiança. Nosso objetivo é sempre proporcionar às
+            pessoas um meio de transporte ágil e seguro, com custos acessíveis,
+            para que possam chegar ao seu destino de forma rápida e segura. Para
+            isso, fazemos uso da tecnologia mais moderna, oferecendo aos nossos
+            clientes soluções inovadoras para suas necessidades de transporte. A
+            Venture Transporte acredita que o transporte deve ser mais do que um
+            serviço, mas sim uma experiência. Acreditamos que o transporte deve
+            ser mais do que um simples meio de locomoção, mas sim uma forma de
+            conectar pessoas e destinos, criando memórias e conexões que duram
+            para sempre. Nossa missão é criar experiências de transporte que
+            possam ser motivo de orgulho para nossos clientes, colaboradores e
+            comunidade, promovendo a segurança, satisfação e confiança de todos.
+          </TextContent>
+        </Separator>
       </Content>
       <Content>
-        <Image />
-        <TitleContent></TitleContent>
-        <TextContent></TextContent>
+        <Separator>
+          <TitleContent>Visão</TitleContent>
+          <TextContent>
+            A Venture Transporte tem como visão ser a melhor empresa de
+            transporte de passageiros, com serviços de qualidade, segurança e
+            responsabilidade socioambiental. Nossa missão é oferecer serviços de
+            transporte de passageiros de forma segura, confortável e eficiente,
+            buscando sempre a satisfação dos nossos clientes. Nossa empresa tem
+            como objetivo fornecer serviços de transporte de passageiros com
+            segurança, conforto e responsabilidade socioambiental. Buscamos
+            oferecer serviços de qualidade, segurança e responsabilidade social,
+            para que nossos passageiros se sintam seguros e confortáveis. Além
+            de investir constantemente na qualidade dos nossos serviços, também
+            buscamos minimizar o nosso impacto no meio ambiente. Estamos
+            comprometidos em usar combustíveis e tecnologias limpas para
+            minimizar o nosso impacto ambiental. Para atingir nossos objetivos,
+            temos uma equipe de profissionais altamente qualificados e treinados
+            para oferecer o melhor serviço possível. Nossa equipe trabalha em
+            conjunto para garantir a satisfação de nossos clientes, oferecendo
+            serviços de qualidade e segurança. Nosso objetivo é tornar a Venture
+            Transporte a melhor empresa de transporte de passageiros do país.
+            Ofereceremos serviços de qualidade, segurança e responsabilidade
+            socioambiental para que nossos clientes tenham sempre uma
+            experiência positiva.
+          </TextContent>
+        </Separator>
+        <Image src="http://static1.squarespace.com/static/507ff40ae4b0f60e9fd2d711/t/59ef4d75dc2b4ae4854eb45e/1487697611950/?format=1500w" />
+      </Content>
+      <Content>
+        <Image src="https://www.siteware.com.br/wp-content/uploads/2021/03/5898774-scaled.jpg" />
+        <Separator>
+          <TitleContent>Valores</TitleContent>
+          <TextContent>
+            A Venture Transporte é uma empresa de transporte que acredita na
+            união de pessoas e na realização de sonhos. Nossos valores refletem
+            essa visão e nos esforçamos para fornecer transporte de qualidade
+            para que nossos clientes possam se sentir seguros e confiantes ao
+            usufruir de nossos serviços. Nossa principal prioridade é a
+            segurança, desde a condução de nossos veículos até a forma como
+            tratamos nossos clientes. Estamos comprometidos em garantir que
+            todos os nossos motoristas sejam profissionais e experientes,
+            alocando os veículos adequados para o transporte de passageiros.
+            Também nos empenhamos em oferecer serviços de alta qualidade e
+            preços justos, sempre mantendo a responsabilidade ambiental, com
+            veículos modernos e eficientes em combustível. Estamos sempre em
+            busca de maneiras de melhorar nossos serviços para atender aos
+            nossos clientes. Por último, mas não menos importante, acreditamos
+            que, trabalhando juntos, podemos realizar sonhos. Acreditamos que
+            nossos serviços podem ajudar nossos clientes a alcançar seus
+            objetivos, fornecendo transporte confiável e seguro. É por isso que
+            nos esforçamos para fornecer o melhor em serviços de transporte.
+          </TextContent>
+        </Separator>
       </Content>
     </Container>
   );
--- src/Components/CardValores/styles.tsx
+++ src/Components/CardValores/styles.tsx
@@ -3,15 +3,39 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   padding: 12px;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
 `;
 export const Content = styled.div`
   width: 100%;
-  margin-bottom: 20px;
+  margin: 20px 0;
+  display: flex;
+  border-radius: 10px;
+  background: rgb(236, 236, 236);
 
-  border: solid 1px;
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
+    rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
 `;
 export const Image = styled.img`
-    
+  width: 30%;
+  padding: 10px;
 `;
-export const TitleContent = styled.h3``;
-export const TextContent = styled.p``;
+export const Separator = styled.div`
+  width: auto;
+  display: flex;
+  flex-direction: column;
+`;
+export const TitleContent = styled.h3`
+  padding: 10px;
+  width: 100%;
+  font-size: 27px;
+  text-align: center;
+  text-transform: uppercase;
+`;
+export const TextContent = styled.p`
+  padding: 10px;
+  width: 100%;
+  font-size: 18px;
+  text-align: justify;
+`;
